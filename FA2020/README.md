40238bdcb611d783928776d7b7e8a1a579d8cd20
mickayla32 <mickayla.johnston001@umb.edu>
2020-09-10 12:50:29 -0400

    new project for class

---
+++ FA2020/README.md
@@ -0,0 +1,2 @@
+# bio607
+Repository for Fall 2020 BIO607 work
---
+++ FA2020/in_class_files/week1/2020_09_10_my_first_r_script.R
@@ -0,0 +1,24 @@
+###########################
+#' 2020_09_10_my_first_file.R
+#' 
+#' This script is my first R file
+#' and it doesn't do much, but it's mine.
+#'
+#' @author Mickayla Johnston
+#'
+#' @changelog
+
+###########################
+
+# Square roots
+sqrt(4)
+
+# Logs
+log10(10)
+
+log(5)
+
+# Natural log
+??"log10"
+
+log2(50)
